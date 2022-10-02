216821482bec6dd86fcdb8830311b13f3c2c7933
Duaij <Daljeemaz@gmail.com>
2022-10-02 20:03:10 +0300

    complete the hr_pro assessment

--- hr_pro.py
+++ hr_pro.py
@@ -1,11 +1,44 @@
 class Employee:
-   #Employee class here
+  def __init__(self, name, age, salary, employment_years, get_annual_salary):
+    self.name = name
+    self.age = age
+    self.salary = salary
+    self.employment_years = employment_years
+    self.get_annual_salary = get_annual_salary
+    return self.salary * 12
 
+  def __str__(self):
+      return f"my name is: {self.name}, my age is: {self.age}years old, my salary is: {self.salary}, working years: {self.employment_years}, and salary is: {self.get_annual_salary}"
+  
 class Manager(Employee):
-    #Manager class here
+    def manger(self, name, age, salary, employment_years, bonus_percentage, get_bonus):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.employment_years = employment_years
+        self.bonus_percentage = bonus_percentage
+        self.get_bonus = get_bonus
+        return self.bonus_percentage * salary
+
+    def __str__(self):
+        return f"my name is: {self.name}, my age is: {self.age}years old, my salary is: {self.salary}, working years: {self.employment_years}, and bonus is: {self.get_bonus}"
+
+employee = ["name", "age", "salary", "years", "annual_salary"]
+manager = ["name", "age", "salary", "years", "bonus_percentage", "bonus"]
+
+
+
         
 def main():
-	# main code here
+    print("Welcome to the HR Pro")
+    print("option:" )
+    print(Employee)
+    for p1 in employee:
+        print(input(employee))
+    print(Manager)
+    for p2 in manager:
+        print(input(manager))
+    return main
+if __name__ == '__main__.Employee':
 
-if __name__ == '__main__':
-	main()
+    main()
